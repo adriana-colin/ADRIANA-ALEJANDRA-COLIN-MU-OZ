f6bccde712170953292b92008dcc737c6fdc9eda
Adriana Colin <adriana_3127230713@utd.edu.mx>
2024-05-08 13:34:05 -0600

    Ejercicios sesion 1

---
+++ area_circulo.py
@@ -0,0 +1,6 @@
+import math
+
+radio = float(input("Ingresa la medida del radio: "))
+area = math.pi * radio ** 2
+
+print("El área es de: ", area)
---
+++ area_cuadrado.py
@@ -0,0 +1,6 @@
+base = float(input("Ingresa el valor de la base: "))
+altura = float(input("Ingresa el valor de la altura: "))
+
+area = base * altura
+
+print("El valor del área es: ", area)
